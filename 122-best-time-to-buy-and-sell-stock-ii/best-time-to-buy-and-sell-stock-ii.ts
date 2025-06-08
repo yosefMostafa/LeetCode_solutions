0fb5ed2ebde9f587aacb22882f1f0628f84f40ea
Yosef Mostafa <58142372+yosefMostafa@users.noreply.github.com>
2025-06-08 18:12:32 +0300

    Time: 2 ms (36.79%) | Memory: 55.8 MB (78.23%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.ts
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.ts
@@ -1,10 +1,9 @@
 function maxProfit(prices: number[]): number {
-    let minPrice = Infinity;
     let totaProfit: number = 0;
     for (let i = 1; i < prices.length; i++) {
-        let price: number = prices[i]
-        let prvPrice: number = prices[i - 1]
-        const profit = price - prvPrice;
+        // let price: number = prices[i]
+        // let prvPrice: number = prices[i - 1]
+        const profit =  prices[i] -  prices[i - 1];
         if (profit > 0) {
             totaProfit += profit;
         }
