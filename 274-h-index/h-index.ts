b8750f14530acf063b35abcf9afe2c25f4dba7af
Yosef Mostafa <58142372+yosefMostafa@users.noreply.github.com>
2025-06-10 18:12:04 +0300

    Time: 3 ms (30.39%) | Memory: 55.7 MB (50.31%) - LeetSync

--- 274-h-index/h-index.ts
+++ 274-h-index/h-index.ts
@@ -2,14 +2,10 @@ function hIndex(citations: number[]): number {
     let hIndex:number = 0;
     citations.sort(function(a, b){return b-a})
     let maxIndex = citations.length
-    console.log(citations)
-    for (let index :number = 0;index < maxIndex;index++){
-           
+    for (let index :number = 0;index < maxIndex;index++){   
         if(citations[index] > hIndex){
             hIndex = index +1
         }
-         console.log(hIndex)
     }
-     console.log(hIndex)
     return hIndex;
 };
