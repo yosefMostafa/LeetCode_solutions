b91db9d58dea78cc2802f729e07c4f9fd72d6bff
Yosef Mostafa <58142372+yosefMostafa@users.noreply.github.com>
2025-06-10 16:39:42 +0300

    Time: 1 ms (80.37%) | Memory: 57.2 MB (56.16%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.ts
+++ 45-jump-game-ii/jump-game-ii.ts
@@ -1,7 +1,6 @@
 function jump(nums: number[]): number {
   let pos: number = 0;
   let jumps: number = 0;
-  const path: number[] = [0];
   if(nums.length <= 1  ){
     return 0;
   }
@@ -18,8 +17,7 @@ function jump(nums: number[]): number {
     }
     // Find the position that can reach farthest within current jump range
     for (let i = pos + 1; i <= Math.min(pos + nums[pos], nums.length - 1); i++) {
-      if (i + nums[i] > farthest) {
-        
+      if (i + nums[i] > farthest) {   
         farthest = i + nums[i];
         nextPos = i;
       }
@@ -27,7 +25,6 @@ function jump(nums: number[]): number {
     
     pos = nextPos;
     jumps++;
-    path.push(pos);
   }
   return jumps;
 };
