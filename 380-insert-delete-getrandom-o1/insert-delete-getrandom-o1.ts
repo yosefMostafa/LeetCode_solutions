52cb857a32bb7881157eed3918b22f334f34b017
Yosef Mostafa <58142372+yosefMostafa@users.noreply.github.com>
2025-06-12 22:01:24 +0300

    Time: 68 ms (89.28%) | Memory: 108.1 MB (50.75%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.ts
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.ts
@@ -1,17 +1,20 @@
 class RandomizedSet {
     private valToIndex: Map<number, number>;
     private values: number[];
+    private length :number;
 
     constructor() {
         this.valToIndex = new Map();
         this.values = [];
+        this.length = 0;
     }
 
     insert(val: number): boolean {
         if (this.valToIndex.has(val)) return false;
 
         this.values.push(val);
-        this.valToIndex.set(val, this.values.length - 1);
+        this.length++;
+        this.valToIndex.set(val, this.length - 1);
         return true;
     }
 
@@ -19,19 +22,20 @@ class RandomizedSet {
         if (!this.valToIndex.has(val)) return false;
 
         const indexToRemove = this.valToIndex.get(val)!;
-        const lastElement = this.values[this.values.length - 1];
+        const lastElement = this.values[this.length - 1];
 
         this.values[indexToRemove] = lastElement;
         this.valToIndex.set(lastElement, indexToRemove);
 
         this.values.pop();
+        this.length --;
         this.valToIndex.delete(val);
 
         return true;
     }
 
     getRandom(): number {
-        const randIndex = Math.floor(Math.random() * this.values.length);
+        const randIndex = Math.floor(Math.random() *this.length);
         return this.values[randIndex];
     }
 }
