7adb5d8c284d74d2ef04c8d5955c92c81863757f
Yosef Mostafa <58142372+yosefMostafa@users.noreply.github.com>
2025-06-15 19:43:31 +0300

    Time: 8 ms (13.63%) | Memory: 70.1 MB (8.08%) - LeetSync

--- 134-gas-station/gas-station.ts
+++ 134-gas-station/gas-station.ts
@@ -1,18 +1,20 @@
 function canCompleteCircuit(gas: number[], cost: number[]): number {
     let sumGas : number = 0;
     let sumCost :number =0 ;
-    for(let i:number = 0;i<gas.length;i++){
-        sumGas +=gas[i]
-        sumCost += cost[i]
-    }
-    if(sumGas < sumCost){
-        return -1;
-    }    
+    // for(let i:number = 0;i<gas.length;i++){
+    //     sumGas +=gas[i]
+    //     sumCost += cost[i]
+    // }
+    // if(sumGas < sumCost){
+    //     return -1;
+    // }    
     let carTank : number = 0;
     let path:number[] = [];
     for(let i:number = 0;i<gas.length;i++){
         carTank += gas[i]
         carTank = carTank - cost[i]
+             sumGas +=gas[i]
+        sumCost += cost[i]
         if(carTank >= 0){
            path.push(i)
         }else{
@@ -20,8 +22,12 @@ function canCompleteCircuit(gas: number[], cost: number[]): number {
             path=[]
         }
     }
- 
-  
+  if(sumGas < sumCost){
+        return -1;
+    }   
+    if(path .length > 0){
         return path[0] 
-    
+    }else{
+        return -1
+    }
 };
