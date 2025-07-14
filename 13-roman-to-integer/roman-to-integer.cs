2299a01ec6fa23a619f2882b469731b773ddec35
Yosef Mostafa <58142372+yosefMostafa@users.noreply.github.com>
2025-07-14 16:11:55 +0300

    Time: 8 ms (12.74%) | Memory: 50.7 MB (52.42%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cs
+++ 13-roman-to-integer/roman-to-integer.cs
@@ -1,20 +1,23 @@
 public class Solution {
-    private Dictionary<char, int> romanToValue = new Dictionary<char, int>(); 
-    public Solution(){
-        romanToValue.Add('I', 1);
+
+    private Dictionary<char, int> getRomansDic(){
+        Dictionary<char, int> romanToValue = new Dictionary<char, int>();
+         romanToValue.Add('I', 1);
         romanToValue.Add('V', 5);
         romanToValue.Add('X', 10);
         romanToValue.Add('L', 50);
         romanToValue.Add('C', 100);
         romanToValue.Add('D', 500);
         romanToValue.Add('M', 1000);
+        return romanToValue;
+
     }
 
     public int RomanToInt(string s) {
         int result = 0;
         int i = 0;
+        Dictionary<char, int> romanToValue = getRomansDic();
         while(i < s.Length){
-       
             if(i != s.Length -1){
                 if(romanToValue[s[i+1]] >romanToValue[s[i]]){
                     result += romanToValue[s[i+1]] - romanToValue[s[i]];
@@ -26,6 +29,7 @@ public class Solution {
             i++;
            
         }
+        romanToValue.Clear();
         return result; 
     }
 }
