c2de3251092da572052644631a1a214578b13bf1
Yosef Mostafa <58142372+yosefMostafa@users.noreply.github.com>
2025-07-26 19:06:23 +0300

    Time: 1 ms (84.47%) | Memory: 43.5 MB (86.03%) - LeetSync

--- 242-valid-anagram/valid-anagram.cs
+++ 242-valid-anagram/valid-anagram.cs
@@ -2,24 +2,19 @@ public class Solution {
     public bool IsAnagram(string s, string t) {
         if(s.Length != t.Length)
             return false;
-        int[] sCharCount = getCharCount(s);
-        // int[] tCharCount = getCharCount(t);
-        int[] charCount = new int[26];
+        int[] sCharCount = new int[26];
+        foreach(char i in s){
+            sCharCount[i - 'a'] ++;
+        }
+        int[] tcharCount = new int[26];
         foreach(char i in t){
-             charCount[i - 'a']++;
-            if(sCharCount[i - 'a'] == 0 || charCount[i - 'a'] > sCharCount[i - 'a'])
+            tcharCount[i - 'a']++;
+            if(sCharCount[i - 'a'] == 0 || tcharCount[i - 'a'] > sCharCount[i - 'a'])
                 return false;
-             
                
         } 
         return true;
         
     }
-    private int[] getCharCount(string word){
-        int[] charCount = new int[26];
-        foreach(char i in word){
-            charCount[i - 'a'] ++;
-        }
-        return charCount;
-    } 
+
 }
