aff96e870a402f1c2692ba65aa7e931da089e0d9
Yosef Mostafa <58142372+yosefMostafa@users.noreply.github.com>
2025-07-26 19:05:00 +0300

    Time: 1 ms (84.47%) | Memory: 43.6 MB (86.03%) - LeetSync

--- 242-valid-anagram/valid-anagram.cs
+++ 242-valid-anagram/valid-anagram.cs
@@ -3,10 +3,14 @@ public class Solution {
         if(s.Length != t.Length)
             return false;
         int[] sCharCount = getCharCount(s);
-        int[] tCharCount = getCharCount(t);
-        foreach(char i in s){
-            if(sCharCount[i - 'a'] != tCharCount[i - 'a'])
+        // int[] tCharCount = getCharCount(t);
+        int[] charCount = new int[26];
+        foreach(char i in t){
+             charCount[i - 'a']++;
+            if(sCharCount[i - 'a'] == 0 || charCount[i - 'a'] > sCharCount[i - 'a'])
                 return false;
+             
+               
         } 
         return true;
         
