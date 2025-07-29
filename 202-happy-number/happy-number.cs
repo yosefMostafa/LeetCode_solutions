4cad3ed7cb38fe8d4b397e0b018de9ead22ff8e2
Yosef Mostafa <58142372+yosefMostafa@users.noreply.github.com>
2025-07-29 14:48:48 +0300

    Time: 0 ms (100.00%) | Memory: 31.1 MB (78.86%) - LeetSync

--- 202-happy-number/happy-number.cs
+++ 202-happy-number/happy-number.cs
@@ -1,21 +1,46 @@
 public class Solution {
-    public bool IsHappy(int n) {
-        string InitialNum = n.ToString();
-        HashSet<int> seen = new HashSet<int>();
-        while(true){
+    // public bool IsHappy(int n) {
+    //     string InitialNum = n.ToString();
+    //     HashSet<int> seen = new HashSet<int>();
+    //     while(true){
         
-        int sum = 0;
-        for (int i = 0 ;i< InitialNum.Length;i++){
-            int num = int.Parse(InitialNum[i].ToString());
-            sum += num * num;
-        }
-        if(sum == 1)
-            return true; 
-        if(!seen.Add(sum))
-            return false;
+    //     int sum = 0;
+    //     for (int i = 0 ;i< InitialNum.Length;i++){
+    //         int num = int.Parse(InitialNum[i].ToString());
+    //         sum += num * num;
+    //     }
+    //     if(sum == 1)
+    //         return true; 
+    //     if(!seen.Add(sum))
+    //         return false;
        
-        InitialNum = sum.ToString();
-        }
+    //     InitialNum = sum.ToString();
+    //     }
        
+    // }
+    private int GetSumOfSquares(int n)
+{
+    int sum = 0;
+    while (n > 0)
+    {
+        int digit = n % 10;
+        sum += digit * digit;
+        n /= 10;
     }
+    return sum;
+}
+
+public bool IsHappy(int n)
+{
+    int slow = n;
+    int fast = GetSumOfSquares(n);
+
+    while (fast != 1 && slow != fast)
+    {
+        slow = GetSumOfSquares(slow);             // move 1 step
+        fast = GetSumOfSquares(GetSumOfSquares(fast)); // move 2 steps
+    }
+
+    return fast == 1;
+}
 }
