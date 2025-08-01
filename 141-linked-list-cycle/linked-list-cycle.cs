a5f9852908a53881f392c71a029a3dd33cf51f18
Yosef Mostafa <58142372+yosefMostafa@users.noreply.github.com>
2025-08-01 16:50:16 +0300

    Time: 95 ms (87.95%) | Memory: 47.4 MB (83.20%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cs
+++ 141-linked-list-cycle/linked-list-cycle.cs
@@ -11,16 +11,29 @@
  */
 public class Solution {
     public bool HasCycle(ListNode head) {
-       if(head == null)
-            return false;
-        HashSet<ListNode> nodesHandler = new HashSet<ListNode>();
-        ListNode node = head;
-        while(node != null){
-            if(!nodesHandler.Add(node))
-                return true;
-            node = node.next;
+         if (head == null || head.next == null) return false;
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;           // move 1 step
+            fast = fast.next.next;      // move 2 steps
+            
+            if (slow == fast) return true; // cycle detected
         }
-        return false;
+        
+        return false; // no cycle
+    //    if(head == null)
+    //         return false;
+    //     HashSet<ListNode> nodesHandler = new HashSet<ListNode>();
+    //     ListNode node = head;
+    //     while(node != null){
+    //         if(!nodesHandler.Add(node))
+    //             return true;
+    //         node = node.next;
+    //     }
+    //     return false;
         
     }
 }
